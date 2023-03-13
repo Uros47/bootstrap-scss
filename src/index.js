f58ac339ea88ae6f4b267f77a3097d7ae221a3f6
UrosD <urosdelic89@gmail.com>
2023-03-13 01:58:53 +0100

    modal works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-const testButton = () => {
-  alert("test");
-};
+let modal = document.getElementById("modal-container");
+let closeBtn = document.getElementById("closeBtn");
+
+if (closeBtn) {
+  closeBtn.addEventListener("click", closeModal);
+}
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
